0a0943a1fe02aff1a9a4ba15cb8a7af44e3e6e09
rohitpatil4157 <72158901+rohitpatil4157@users.noreply.github.com>
2022-07-18 21:10:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ showNote();
 clipBoard();
 
 let Music = new Audio('./Popup.mp3')
-let Whoosh = new Audio('./Wooosh2.wav');
+let Whoosh = new Audio('./Wooosh3.wav');
 let addBtn = document.querySelector('#addBtn');
 
 
